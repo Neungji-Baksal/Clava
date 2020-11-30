33d6272562690b4ce98acad4bf1833c94db6b13f
Neungji-Baksal <57937528+Neungji-Baksal@users.noreply.github.com>
2020-11-30 13:24:33 +0900

    수정사항 발생

--- clava-customer-api/src/main/java/kr/co/claveteam/Clava/application/NickNameExistedException.java
+++
@@ -1,8 +0,0 @@
-package kr.co.claveteam.Clava.application;
-
-public class NickNameExistedException extends RuntimeException{
-
-    NickNameExistedException(String nickname){
-        super("이미 등록된 아이디 입니다.");
-    }
-}
